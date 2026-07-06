72e03ca45759319923e4d8625202ff171cf80a4a
Girish Lade <girishlade111@gmail.com>
2026-07-06 02:15:39 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.paperknife.app',
+  appName: 'LS PDF',
+  webDir: 'dist'
+};
+
+export default config;
